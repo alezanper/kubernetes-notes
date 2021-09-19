3763d26a650378af802aca875018fdf1e6b7b089
fabenavideso@gmail.com <fabenavideso@gmail.com>
2021-09-18 22:12:29 -0500

    [added] infrastructure script

---
+++ AKS/application_gateway/create_infrastructure.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ProjectName="myproject"
+ResourceGroupName="myrg"
+Location="westus2"
+Dns="mysuperdns2030"
+
+# Create Resource Group
+az group create \
+    -n $ResourceGroupName -l $Location
+
+# Create Ip Adress
+az network public-ip create \
+    -n "$ProjectName-pip" -g $ResourceGroupName --allocation-method Static --sku Standard --dns-name $Dns
+
+# Create Vnet and Subnet
+az network vnet create \
+    -n "$ProjectName-vnet" -g $ResourceGroupName --address-prefix 192.168.0.0/24 --subnet-name "$ProjectName-subnet" --subnet-prefix 192.168.0.0/24
+
+# Create Application Gateway
+az network application-gateway create \
+    -n "$ProjectName-gateway" -l $Location -g $ResourceGroupName --sku Standard_v2 --public-ip-address "$ProjectName-pip" --vnet-name "$ProjectName-vnet" --subnet "$ProjectName-subnet"
+
+# Get Gateway Id
+appgwId=$(az network application-gateway show -n "$ProjectName-gateway" -g $ResourceGroupName -o tsv --query "id")
+
+
+
+# bash create_infrastructure.sh
