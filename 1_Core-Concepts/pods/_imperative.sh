7b426bd9d4c95dd761029be5e40ddfcda496d028
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-01 19:41:53 -0500

    imperative commands for pods

--- 1_Core-Concepts/pods/_imperative.sh
+++ 1_Core-Concepts/pods/_imperative.sh
@@ -2,16 +2,17 @@
 ## Pods ##
 ##########
 
-# Create a pod
+# Create a pod from an image
 kubectl run nginx --image=nginx
 kubectl run httpd --image=httpd:alpine
 
-# Generate manifest (pod.yaml) using commands
+# Generate manifest (pod.yaml) using imperative command
 kubectl run nginx --image=nginx --dry-run=client -o yaml > pod.yaml
 kubectl run redis --image=redis:alpine --dry-run=client -o yaml > redis.yaml
 
 # Delete a pod
 kubectl delete pod nginx
 
+# get the template from a running pod
 kubectl get pod webapp -o yaml > my-new-pod.yaml
 
