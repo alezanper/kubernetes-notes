0ab45d3bae99e6b53349e83b4df29d00ee88807d
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-01 19:55:29 -0500

    updated deployment imperative

--- 1_Core-Concepts/deployment/_imperative.sh
+++ 1_Core-Concepts/deployment/_imperative.sh
@@ -1,14 +1,18 @@
+#################
+## Deployments ##
+#################
+
 # Create a deployment
-kubectl create deployment --image=nginx nginx
+kubectl create deployment nginx --image=nginx
  
 # Generate Deployment with 4 Replicas
 kubectl create deployment nginx --image=nginx --replicas=4
  
-You can also scale a deployment using the kubectl scale command.
+# You can also scale a deployment using the kubectl scale command.
 kubectl scale deployment nginx --replicas=4
 
 # Generate Deployment YAML file (-o yaml). Don't create it (--dry-run)
-kubectl create deployment --image=nginx nginx --dry-run -o yaml > deployment.yaml
+kubectl create deployment --image=nginx nginx --dry-run=client -o yaml > deployment.yaml
 
 # Create a deployment on a specific namespace
 kubectl create deployment redis-deploy --image=redis --replicas=2 --namespace=dev-ns
