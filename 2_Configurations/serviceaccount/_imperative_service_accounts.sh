e37bf4d17514cb9daeed984131b8ec3fc1447854
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-08 21:18:17 -0500

    [updated] resources and service accounts

---
+++ 2_Configurations/serviceaccount/_imperative_service_accounts.sh
@@ -0,0 +1,6 @@
+# Service accounts	for machines
+# user accounts		for people and developers
+
+kubectl create serviceaccount <myservice-account-name>
+kubectl get serviceaccount
+kubectl describe serviceaccount <myservice-account-name>
