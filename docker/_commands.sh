e5feb82b80fbc43172eb88cb7a9a0eec346503b5
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-10 17:04:18 -0500

    [added] additionals

---
+++ docker/_commands.sh
@@ -0,0 +1,20 @@
+# to build an app using a Dockerfile
+docker build Dockerfile -t alex/myapp
+
+# To push an image to docker hub
+docker push alex/myapp
+
+# for getting information about the layers on the app
+docker history alex/myapp
+
+# List docker running
+docker ps
+
+# List local images
+docker images
+
+# Run nginx and expose port 8080 (container) to 8282 (host)
+docker run nginx:latest -p 8282:8080
+
+# Run a python container
+docker run python:3.6
