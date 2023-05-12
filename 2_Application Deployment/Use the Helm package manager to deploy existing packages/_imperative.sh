d4fd9ce026f161c08751b264ee40e568ceed08a7
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-11 19:45:19 -0500

    [updated] helm commands

---
+++ 2_Application Deployment/Use the Helm package manager to deploy existing packages/_imperative.sh
@@ -0,0 +1,32 @@
+# install helm
+sudo snap install helm --classic
+
+# List helm releases
+helm -n <namespace> ls
+
+# Delete (unistall) releases 
+helm -n <namespace> uninstall <release-name> 
+
+# Helm repo list
+helm repo list
+
+# Helm repo update
+helm repo update
+
+# Search an helm repo
+helm search repo nginx
+
+# upgrade a release
+helm -n <namespace> upgrade <release> bitnami/nginx
+
+# Check values from a package
+helm show values bitnami/apache
+helm show values bitnami/apache | yq e
+
+# install a customize version of apache
+helm -n <namespace> install <release-name> bitnami/apache --set replicaCount=2 --set image.debug=true
+
+# List all releases even the broken ones
+helm -n <namespace> ls -a
+
+
