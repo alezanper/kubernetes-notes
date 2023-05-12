124fd8b5fd512a96d8288faea45baf19c2dc79f1
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-11 19:26:27 -0500

    [updated] job commands

---
+++ 1_Application Design and Build/Understand Jobs and CronJobs/_imperative.sh
@@ -0,0 +1,9 @@
+# create a job with specific command and image
+export do="--dry-run=client -o yaml" 
+kubectl -n <namespace> create job <jobname> --image=busybox:1.31.0 $do > file -- sh -c "sleep 2 && echo done"
+
+# get pod and job associated
+kubectl -n <namespace> get pod, job | grep <jobname>
+
+# describe job
+kubectl -n <namespace> describe job <jobname>
