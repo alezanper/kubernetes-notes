b91ef5a2cb5a74dd2544ddb32663bee04d291e49
fabenavideso@gmail.com <fabenavideso@gmail.com>
2023-05-11 20:07:53 -0500

    [updated] service account

--- 4_Application Environment, Configuration and Security/Understand ServiceAccounts/_imperative_service_accounts.sh
+++ 4_Application Environment, Configuration and Security/Understand ServiceAccounts/_imperative_service_accounts.sh
@@ -4,3 +4,13 @@
 kubectl create serviceaccount <myservice-account-name>
 kubectl get serviceaccount
 kubectl describe serviceaccount <myservice-account-name>
+
+# Since k8s 1.24 secrets won't be created for service accounts automatically
+# If a secret belongs to a service account it'll have the annotation kubernetes.io/service-account.name
+kubectl -n <namespace> get secrets -o yaml | grep annotations -A 1 
+
+# Get token encoded
+kubectl -n <namespace> get secret <secret-associated-sa> -o yaml
+
+# Get decoded token
+kunectl -n <namespace> describe secret <secret-associated-sa>
