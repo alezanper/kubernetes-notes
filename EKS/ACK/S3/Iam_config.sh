d31c8a20aae48ef5aecfa66ac22bcb47a3ed41d9
Alex <fabenavideso@gmail.com>
2023-08-07 12:10:50 -0500

    [added] ack for s3 notes

---
+++ EKS/ACK/S3/Iam_config.sh
@@ -0,0 +1,118 @@
+###IAM###
+
+# Add OIDC
+export EKS_CLUSTER_NAME=<eks cluster name>
+export AWS_REGION=<aws region id>
+eksctl utils associate-iam-oidc-provider --cluster $EKS_CLUSTER_NAME --region $AWS_REGION --approve
+
+CREATE ROLE FOR ACK SERVICE CONTROLLER
+# Update the service name variables as needed
+SERVICE="s3"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+OIDC_PROVIDER=$(aws eks describe-cluster --name $EKS_CLUSTER_NAME --region $AWS_REGION --query "cluster.identity.oidc.issuer" --output text | sed -e "s/^https:\/\///")
+ACK_K8S_NAMESPACE=ack-system
+
+ACK_K8S_SERVICE_ACCOUNT_NAME=ack-$SERVICE-controller
+
+read -r -d '' TRUST_RELATIONSHIP <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${AWS_ACCOUNT_ID}:oidc-provider/${OIDC_PROVIDER}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${OIDC_PROVIDER}:sub": "system:serviceaccount:${ACK_K8S_NAMESPACE}:${ACK_K8S_SERVICE_ACCOUNT_NAME}"
+        }
+      }
+    }
+  ]
+}
+EOF
+echo "${TRUST_RELATIONSHIP}" > trust.json
+
+ACK_CONTROLLER_IAM_ROLE="ack-${SERVICE}-controller"
+ACK_CONTROLLER_IAM_ROLE_DESCRIPTION="IRSA role for ACK ${SERVICE} controller deployment on EKS cluster using Helm charts"
+aws iam create-role --role-name "${ACK_CONTROLLER_IAM_ROLE}" --assume-role-policy-document file://trust.json --description "${ACK_CONTROLLER_IAM_ROLE_DESCRIPTION}"
+ACK_CONTROLLER_IAM_ROLE_ARN=$(aws iam get-role --role-name=$ACK_CONTROLLER_IAM_ROLE --query Role.Arn --output text)
+
+
+ATTACH POLICY TO THE ROLE
+
+# Download the recommended managed and inline policies and apply them to the
+# newly created IRSA role
+BASE_URL=https://raw.githubusercontent.com/aws-controllers-k8s/${SERVICE}-controller/main
+POLICY_ARN_URL=${BASE_URL}/config/iam/recommended-policy-arn
+POLICY_ARN_STRINGS="$(wget -qO- ${POLICY_ARN_URL})"
+
+INLINE_POLICY_URL=${BASE_URL}/config/iam/recommended-inline-policy
+INLINE_POLICY="$(wget -qO- ${INLINE_POLICY_URL})"
+
+while IFS= read -r POLICY_ARN; do
+    echo -n "Attaching $POLICY_ARN ... "
+    aws iam attach-role-policy \
+        --role-name "${ACK_CONTROLLER_IAM_ROLE}" \
+        --policy-arn "${POLICY_ARN}"
+    echo "ok."
+done <<< "$POLICY_ARN_STRINGS"
+
+if [ ! -z "$INLINE_POLICY" ]; then
+    echo -n "Putting inline policy ... "
+    aws iam put-role-policy \
+        --role-name "${ACK_CONTROLLER_IAM_ROLE}" \
+        --policy-name "ack-recommended-policy" \
+        --policy-document "$INLINE_POLICY"
+    echo "ok."
+fi
+
+
+ASSOCIATE ROLE TO SERVICE ACCOUNT 
+
+kubectl describe serviceaccount/$ACK_K8S_SERVICE_ACCOUNT_NAME -n $ACK_K8S_NAMESPACE
+
+# Annotate the service account with the ARN
+export IRSA_ROLE_ARN=eks.amazonaws.com/role-arn=$ACK_CONTROLLER_IAM_ROLE_ARN
+kubectl annotate serviceaccount -n $ACK_K8S_NAMESPACE $ACK_K8S_SERVICE_ACCOUNT_NAME $IRSA_ROLE_ARN
+
+
+
+# Note the deployment name for ACK service controller from following command
+kubectl get deployments -n $ACK_K8S_NAMESPACE
+kubectl -n $ACK_K8S_NAMESPACE rollout restart deployment <ACK deployment name>
+
+
+
+
+kubectl get pods -n $ACK_K8S_NAMESPACE
+kubectl describe pod -n $ACK_K8S_NAMESPACE <NAME> | grep "^\s*AWS_"
+
+
+
+
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+export BUCKET_NAME=my-ack-s3-bucket-$AWS_ACCOUNT_ID
+
+read -r -d '' BUCKET_MANIFEST <<EOF
+apiVersion: s3.services.k8s.aws/v1alpha1
+kind: Bucket
+metadata:
+  name: $BUCKET_NAME
+spec:
+  name: $BUCKET_NAME
+EOF
+
+echo "${BUCKET_MANIFEST}" > bucket.yaml
+
+kubectl create -f bucket.yaml
+
+kubectl describe bucket/$BUCKET_NAME
+
+
+
+https://aws-controllers-k8s.github.io/community/docs/user-docs/install/
+
+https://aws-controllers-k8s.github.io/community/docs/tutorials/rds-example/
---
+++ EKS/ACK/S3/Install_ack.sh
@@ -0,0 +1,12 @@
+# https://aws-controllers-k8s.github.io/community/docs/user-docs/install/
+
+export SERVICE=s3
+export RELEASE_VERSION=$(curl -sL https://api.github.com/repos/aws-controllers-k8s/${SERVICE}-controller/releases/latest | jq -r '.tag_name | ltrimstr("v")')
+export ACK_SYSTEM_NAMESPACE=ack-system
+export AWS_REGION=us-east-1
+
+aws ecr-public get-login-password --region us-east-1 | helm registry login --username AWS --password-stdin public.ecr.aws
+helm install --create-namespace -n $ACK_SYSTEM_NAMESPACE ack-$SERVICE-controller \
+  oci://public.ecr.aws/aws-controllers-k8s/$SERVICE-chart --version=$RELEASE_VERSION --set=aws.region=$AWS_REGION
+
+helm list --namespace $ACK_SYSTEM_NAMESPACE -o yaml
