22d2d1131a47c4a6798a5767b3a1fd9abb4a6b32
Alex <fabenavideso@gmail.com>
2023-08-17 20:33:08 -0500

    [update] cluster creation script

--- EKS/create_cluster.sh
+++ EKS/create_cluster.sh
@@ -1,20 +1,25 @@
+#!/bin/bash
+
 # Previously credentials configured on console aws cli
 # latest version of aws cli
 # the right version of eksctl and kubectl
 
+CLUSTER_NAME=myk8s
+REGION=us-east-1
+
 # Create a cluster without nodegroup
-eksctl create cluster --name=myk8s --region=us-east-1 --zones=us-east-1a,us-east-1b --without-nodegroup 
+eksctl create cluster --name=$CLUSTER_NAME --region=$REGION --zones=us-east-1a,us-east-1b --without-nodegroup 
 
 # Get List of clusters
 eksctl get cluster
 
 # Create and Associate IAM OIDC Provider:
-eksctl utils associate-iam-oidc-provider --region us-east-1 --cluster myk8s --approve
+eksctl utils associate-iam-oidc-provider --region $REGION --cluster $CLUSTER_NAME --approve
 
 # Create Public Node Group   
-eksctl create nodegroup --cluster=myk8s --region=us-east-1 --name=myk8s-ng --node-type=t3.medium --nodes=1 --nodes-min=1 --nodes-max=2 --node-volume-size=20 --managed --asg-access --external-dns-access --full-ecr-access --appmesh-access --alb-ingress-access
+eksctl create nodegroup --cluster=$CLUSTER_NAME --region=us-east-1 --name="$CLUSTER_NAME-ng" --node-type=t3.medium --nodes=1 --nodes-min=1 --nodes-max=2 --node-volume-size=20 --managed --asg-access --external-dns-access --full-ecr-access --appmesh-access --alb-ingress-access
 
 # Get nodes 
 kubectl get nodes -o wide
 
-aws eks update-kubeconfig --region=us-east-1 --name=myk8s
+# aws eks update-kubeconfig --region=us-east-1 --name=myk8s
